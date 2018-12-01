8416b9a971808019c5b10915b66bec9c77bdde7a
Isaac Buitrago <mightyking7@gmail.com>
2018-12-01 00:00:15 -0600

    Created Vertice class for the vertices of a graph

---
+++ Graphs/Vertice.py
@@ -0,0 +1,14 @@
+
+
+'''
+Vertice in a graph
+
+Stroes a value,
+'''
+class Vertice:
+
+    # construct a node with an id and distance
+    def __init__(self, id):
+
+        self.d = 0
+        self.id = id
