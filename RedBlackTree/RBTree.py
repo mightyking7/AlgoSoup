145347f4108915f08881466772c76393a9f71e35
Isaac Buitrago <mightyking7@gmail.com>
2018-12-13 16:36:52 -0600

    Added left and right rotation functions. Added clear function

--- RedBlackTree/RBTree.py
+++ RedBlackTree/RBTree.py
@@ -1,6 +1,6 @@
 
 from Node import *
-import Colors
+from Colors import Color
 
 '''
 Red black tree that stores nodes, performs rotations,
@@ -20,7 +20,17 @@ class RBTree:
 
         self.root = self.nil
 
+    '''
 
+        Purpose:
+          Creates a new node and inserts it into a proper location in the tree
+
+        Parameters:
+            key - value of new node to insert in the tree
+        Notes:
+            Calls insertFixup to maintain the properties of a Red-Black tree
+
+    '''
     def insert(self, key):
 
         y = self.nil
@@ -28,7 +38,7 @@ class RBTree:
         x = self.root
 
         # create the new node
-        z = Node(key, Colors.RED)
+        z = Node(key, Color.RED)
 
         while x != self.nil:
 
@@ -59,39 +69,52 @@ class RBTree:
 
         self.insertFixup(z)
 
+    '''
+        Purpose:
+           Reorders nodes in the tree after insertion
 
+        Parameters:
+            node - newly inserted node in tree
+
+        Notes:
+            Performs re-coloring if uncle and parent of node is red
+            Performs right rotation and left rotation if the new
+            node is in a zig zag location from grandparent
+    '''
     def insertFixup(self, node):
 
-        # while the the parent is red, their may be an imbalance
-        while node.parent.color == Colors.RED:
+        # while the parent is red, their may be an imbalance
+        while node.parent != self.nil and node.parent.color == Color.RED:
 
-            # if on left branch of grandparent
+            # on left branch of grandparent
             if node.parent == node.parent.parent.left:
 
                 y = node.parent.parent.right
 
                 # case 1, recolor parent and uncle
-                if y != self.nil and y.color == Colors.RED:
+                if y != self.nil and y.color == Color.RED:
 
-                    y.color = node.parent.color = Colors.BLACK
+                    y.color = node.parent.color = Color.BLACK
 
-                    node.parent.parent.color = Colors.RED
+                    node.parent.parent.color = Color.RED
 
                     node = node.parent.parent
 
-                #case 2, perform left rotation
-                elif node == node.parent.right:
+                else:
 
-                    node = node.parent
+                    # case 2, perform left rotation
+                    if node == node.parent.right:
 
-                    self.leftRotate(node)
+                        node = node.parent
 
-                # case 3, perform right rotation and recolor nodes
-                node.parent.color = Colors.BLACK
+                        self.leftRotate(node)
 
-                node.parent.parent.color = Colors.RED
+                    # case 3, perform right rotation and recolor nodes
+                    node.parent.color = Color.BLACK
 
-                self.rightRotate(node.parent.parent)
+                    node.parent.parent.color = Color.RED
+
+                    self.rightRotate(node.parent.parent)
 
             # on the right branch of grandparent
             else:
@@ -99,32 +122,141 @@ class RBTree:
                 y = node.parent.parent.left
 
                 # case 1, recolor parent and uncle
-                if y != self.nil and y.color == Colors.RED:
+                if y != self.nil and y.color == Color.RED:
 
-                    y.color = node.parent.color = Colors.BLACK
+                    y.color = node.parent.color = Color.BLACK
 
-                    node.parent.parent.color = Colors.RED
+                    node.parent.parent.color = Color.RED
 
                     node = node.parent.parent
 
-                # case 2, perform left rotation
-                elif node == node.parent.left:
+                else:
 
-                    node = node.parent
+                    # case 2, perform right rotation
+                    if node == node.parent.left:
 
-                    self.rightRotate(node)
+                        node = node.parent
 
-                # case 3, perform right rotation and recolor nodes
-                node.parent.color = Colors.BLACK
+                        self.rightRotate(node)
 
-                node.parent.parent.color = Colors.RED
+                    # case 3, perform left rotation and recolor nodes
+                    node.parent.color = Color.BLACK
 
-                self.leftRotate(node.parent.parent)
+                    node.parent.parent.color = Color.RED
 
-        self.root.color = Colors.BLACK
+                    self.leftRotate(node.parent.parent)
+
+        self.root.color = Color.BLACK
+
+    '''
+        Purpose:
+            Rotates the node to the left of it's children nodes
+
+        Parameters:
+            nodes - node to initiate rotation
+            
+        Notes:
+            Adjusts parent, left, and right references as needed
+    '''
+    def leftRotate(self, node):
+
+        y = node.right
+
+        # set node's new right child
+        node.right = y.left
+
+        if y.left != self.nil:
+
+            y.left.parent = node
+
+        # set parent of y
+        y.parent = node.parent
+
+        if node.parent == self.nil:
+
+            self.root = y
+
+        elif node == node.parent.left:
+
+            node.parent.left = y
+
+        else:
+            node.parent.right = y
+
+        y.left = node
+
+        node.parent = y
+
+    '''
+        Purpose:
+            Rotates the node to the right of it's children nodes
+
+        Parameters:
+            nodes - node to initiate rotation
+            
+        Notes:
+            Adjusts parent, left, and right references as needed
+       '''
+    def rightRotate(self, node):
+
+        # right child of node
+        x = node.left
+
+        # set right child of x to be left of node
+        node.left = x.right
+
+        # set parent of new child
+        x.right.parent = node
+
+        # set parent of x
+        x.parent = node.parent
+
+        # new root
+        if node.parent == self.nil:
+
+            self.root = x
+
+        elif node == node.parent.right:
+
+            node.parent.left = x
+
+        else:
+            node.parent.right = x
 
 
+        x.right = node
 
+        node.parent = x
+
+    '''
+        Purpose
+            Removes the reference counts for each node
+            so Garbage collection can dispose of un-referenced nodes.
+            
+        Parameters:
+            node - reference to node in RBTree
+            
+        Notes:
+            Uses a post-order traversal to remove references
+    '''
+    def clear(self, node):
+
+        if node == self.nil:
+            return
+
+        self.clear(node.left)
+
+        self.clear(node.right)
+
+        node.left = None
+        node.right = None
+
+        if node.parent != None:
+            node.parent = None
+
+        # root node reached
+        else:
+            self.root = None
 
 
 
